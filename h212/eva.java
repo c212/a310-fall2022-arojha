9e70801ae1ca7bac51d567f117594216dff5a0e9
arpanojha <ojhaarpan12@gmail.com>
2023-02-07 21:50:25 -0500

    Create eva.java

---
+++ h212/eva.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import static java.lang.Math.sqrt;
+import java.lang.Math;
+public class eva {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number to take the square root of: ");
+        double number = Double.parseDouble(scanner.nextLine());
+        double original = sqrt(number);
+        System.out.println("The original square root is: " + original);
+        //System.out.println(squareRoot(original, number));
+        double first_guess = ((number/2)+2)/2;
+        System.out.println(sqrtt(number/2,first_guess,number));
+    }
+        public static double sqrtt(double prev,double next,double orig){
+                if(Math.abs(prev-next)<0.05 ){
+                        return next;
+                }
+                else{  System.out.println(next+" "+prev+" "+(next+(orig/next)/2));
+                        return sqrtt(next,(next+(orig/next))/2,orig);
+                }
+}
+    public static String squareRoot(double original, double number) {
+        if ((Math.abs(original - number)) < 0.001) {
+            return "The calculated square root is: " + number;
+        }
+        else {
+            double newNumber = ((number / (number * 0.5)) + (number * 0.5)) / 2;
+            return squareRoot(original, newNumber);
+        }
+    }
+}
