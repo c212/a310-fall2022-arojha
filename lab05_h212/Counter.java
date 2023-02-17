eae956b61a49d593e274364055ff46f4adf47ebe
arpanojha <ojhaarpan12@gmail.com>
2023-02-17 15:33:08 -0500

    Create Counter.java

---
+++ lab05_h212/Counter.java
@@ -0,0 +1,12 @@
+ public class Counter{
+	 private int value;
+	 public int getValue(){
+		 return value;
+	 }
+	 public void click(){
+		 value+=1;
+	 }
+	 public void reset(){
+		 value = 0;
+	 }
+ }
